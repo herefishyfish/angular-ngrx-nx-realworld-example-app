0a13608508c7fce45c497068c3fc001adc92994d
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2025-11-29 21:17:05 +0800

    Initial Commit

---
+++ apps/conduit-nativescript/src/app.routes.ts
@@ -0,0 +1,41 @@
+import { Routes } from '@angular/router';
+import { AuthGuard } from '@realworld/auth/data-access';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('@realworld/home/feature-home').then((m) => m.HomeComponent),
+  },
+  // {
+  //   path: 'login',
+  //   loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.LoginComponent),
+  // },
+  // {
+  //   path: 'register',
+  //   loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.RegisterComponent),
+  // },
+  {
+    path: 'article',
+    loadChildren: () => import('@realworld/articles/article').then((m) => m.ARTICLE_ROUTES),
+  },
+  // {
+  //   path: 'settings',
+  //   loadComponent: () =>
+  //     import('@realworld/settings/feature-settings').then((settings) => settings.SettingsComponent),
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'editor',
+  //   loadChildren: () => import('@realworld/articles/article-edit').then((article) => article.ARTICLE_EDIT_ROUTES),
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('@realworld/profile/feature-profile').then((profile) => profile.PROFILE_ROUTES),
+  // },
+];
---
+++ apps/conduit-nativescript/src/main.ts
@@ -0,0 +1,47 @@
+import {
+  bootstrapApplication,
+  provideNativeScriptHttpClient,
+  provideNativeScriptNgZone,
+  provideNativeScriptRouter,
+  registerElement,
+  runNativeScriptAngularApp,
+} from '@nativescript/angular';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { withInterceptorsFromDi } from '@angular/common/http';
+import { routes } from './app.routes';
+import { AppComponent } from './app.component';
+import { environment } from './environments/environment.prod';
+import { API_URL } from '@realworld/core/http-client/src';
+
+
+/**
+ * Register MasonKit Web components
+ */
+import * as MasonKitWeb from "@triniwiz/nativescript-masonkit/web";
+Object.values(MasonKitWeb).forEach((component: any) => {
+  if (typeof component === "function") {
+    registerElement(
+      component.prototype.constructor.name.toLowerCase(),
+      () => component
+    );
+  }
+});
+
+/**
+ * Disable zone by setting this to true
+ * Then also adjust polyfills.ts (see note there)
+ */
+const EXPERIMENTAL_ZONELESS = false;
+
+runNativeScriptAngularApp({
+  appModuleBootstrap: () => {
+    return bootstrapApplication(AppComponent, {
+      providers: [
+        provideNativeScriptHttpClient(withInterceptorsFromDi()),
+        provideNativeScriptRouter(routes),
+        EXPERIMENTAL_ZONELESS ? provideZonelessChangeDetection() : provideNativeScriptNgZone(),
+        { provide: API_URL, useValue: environment.api_url },
+      ],
+    });
+  },
+});
